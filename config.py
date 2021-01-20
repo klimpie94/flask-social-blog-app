ce37968166d4ea668f4e05d03e0453cb051702d5
Anis <anis_arsenal@hotmail.com>
2021-01-20 23:08:16 +0100

    login-registration-confirmation auth methods

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ class DevelopmentConfig(Config):
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'anisboudih@gmail.com'#os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = '***REMOVED***'#os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI =\
     'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
